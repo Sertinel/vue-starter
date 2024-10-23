7d2ab4949de963e9ffc31ec23435101511cdd8d8
Sertinel <cankalenderr@yandex.com>
2024-10-23 15:47:34 +0200

    v0.0.01(Alfa)

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <button @click="changeTitle">Başlığı Değiştir</button>
+        <input v-model="newItem">
+        <ul>
+          <li v-for="item in items" :key="item.id">
+            {{ item.text }}
+          </li>
+        </ul>
+      </div>
+  </template>
+  
+  <script>
+  // JavaScript kodu buraya yazılır
+  export default {
+    name: 'ComponentName',
+    props: {
+      // Dışarıdan alınan özellikler
+    },
+    data() {
+      return {
+        // Bileşenin verileri
+      }
+    },
+    methods: {
+      // Metodlar
+    }
+  }
+  </script>
+  
+  <style>
+  /* CSS kodları buraya yazılır */
+  </style>
